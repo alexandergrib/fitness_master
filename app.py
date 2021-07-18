9e0515c019a6bdc577d54774ed1bcbaa20dff566
Alex <pioneerko@hotmail.com>
2021-07-18 09:25:46 +0100

    CODE: add "edit workout" functionality

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+from pprint import pprint
+from datetime import datetime
+
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
@@ -34,7 +37,39 @@ def get_workout():
     # categories = list(mongo.db.categories.find().sort("category_name", 1))
     # return render_template("categories.html", categories=categories)
 
-    return render_template("workout.html", workout_lsit=workout_list)
+    return render_template("workout.html", workout_list=workout_list)
+
+
+@app.route("/workout/edit/<workout_id>", methods=["GET", "POST"])
+def edit_workout(workout_id):
+    """Edit existing workout. Takes arguments: [workout_id], query DB, update DB with new data, Returns: [status] """
+    if request.method == "POST":
+        # is_urgent = "on" if request.form.get("is_urgent") else "off"
+        is_completed = True if request.form.get("is_completed") else False
+        is_saved = True if request.form.get("is_saved") else False
+        # print(is_completed)
+        submit = {
+            "workout_name": request.form.get("workout_name"),
+            "workout_sets": request.form.get("workout_sets"),
+            "workout_reps": request.form.get("workout_reps"),
+            "exercise_choices": request.form.getlist("exercise_choices"),
+            "modified_date": datetime.now().strftime("%d/%m/%Y"),
+            'completed': is_completed,
+            'saved': is_saved,
+            "created_by": "admin" #session["user"]
+        }
+
+        # print("submit", submit)
+        mongo.db.routines.update({"_id": ObjectId(workout_id)}, submit)
+        flash("Task Successfully Updated")
+        print("Task Successfully Updated")
+
+    # print(request.form)
+    single_workout = mongo.db.routines.find_one({"_id": ObjectId(workout_id)})
+    # print(single_workout)
+    exercise_list = list(mongo.db.exercises.find())
+    # print(exercise_list)
+    return render_template("edit_workout.html", workout_list=single_workout, exercise_list=exercise_list)
 
 
 if __name__ == "__main__":
