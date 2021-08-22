360769839db5be4731f170f25a9b88c517d6be4e
alexandergrib <pioneerko@hotmail.com>
2021-08-22 07:40:59 +0000

    CODE: create start_exerciseing.js

---
+++ static/js/start_exercising.js
@@ -0,0 +1,9 @@
+var divs = document.getElementsByClassName("workout-exercise-single-overlay");
+for(var i = 0; i < divs.length; i++){
+    //do something to each div like
+    // divs[i].innerHTML = "<i class="fab fa-accusoft"></i>";
+    divs[i].style.display = "block";
+ }
+
+
+ document.querySelector('.workout-exercise-single-wrapper').click();
