9a7ec36f663b27d192c05561aa7bf5dc51ad620d
Alex <pioneerko@hotmail.com>
2021-10-06 17:58:05 +0100

    Cleanup the code

--- static/js/script.js
+++ static/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         showClearBtn: true,
         i18n: {
             done: "Select"
-        } 
+        }
     });
 
     /*
@@ -24,17 +24,16 @@ $(document).ready(function () {
     */
 
     var li_count = 2;
-    $('#add_more').click(function(){
+    $('#add_more').click(function () {
         li_count += 1;
         $("#steps_list li:last").before(
-            "<li style='margin-left:3rem;'><label for=step"+li_count+"><input id=step" + 
-            li_count + " name='steps' minlength='3' maxlength='1500' type='text' class='validate' required>"+
-                          " Next Step <i class=' fas fa-minus-circle ' style='color:red; float:right; font-size:1.1rem; cursor: pointer;'> Remove</i></label> </li>"
+            "<li style='margin-left:3rem;'><label for=step" + li_count + "><input id=step" +
+            li_count + " name='steps' minlength='3' maxlength='1500' type='text' class='validate' required>" +
+            " Next Step <i class=' fas fa-minus-circle ' style='color:red; float:right; font-size:1.1rem; cursor: pointer;'> Remove</i></label> </li>"
         );
-
-        $( "i.fa-minus-circle" ).on( "click", function( event ) {
-            $( event.target ).closest( "li" ).remove();            
-          });
+        $("i.fa-minus-circle").on("click", function (event) {
+            $(event.target).closest("li").remove();
+        });
     });
 });
 
