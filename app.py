cab192beeca1a91d5d10c5a94843e567029f38ff
Alex <pioneerko@hotmail.com>
2021-10-08 15:30:49 +0100

    TEST: test debug is off in production

--- app.py
+++ app.py
@@ -566,4 +566,4 @@ def page_not_found(*args, **kwargs):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
